20c0038f4b9fb2ea15863c7b1d2ce3bc7e1fadbe
mattjuvella <matthew.g.vella.23@um.edu.mt>
2024-10-25 08:38:28 +0200

    start of task 2

---
+++ Task2/build/CMakeFiles/ShoppingItems.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShoppingItems.dir/main.cpp.o"
+  "CMakeFiles/ShoppingItems.dir/main.cpp.o.d"
+  "ShoppingItems"
+  "ShoppingItems.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShoppingItems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
